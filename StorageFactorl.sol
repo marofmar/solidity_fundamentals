721634604d936c6cb9a39ad05f05e24e0d52a7da
marofmar <yujin.mar.chung@gmail.com>
2025-08-17 11:15:49 +0900

    composability learned!

---
+++ StorageFactorl.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import "./SimpleStorage.sol";
+
+contract SolidityFactory {
+    // type visibility name
+    // uint public favoriteNumber
+    SimpleStorage public simpleStorage;
+
+    function createSimpleStorageContract() public {
+        simpleStorage = new SimpleStorage();  // new - solidity knows deploy contract 
+    }
+}
