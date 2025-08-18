73a76fa500bc2d63f64f55befc8fb173e67305ab
marofmar <yujin.mar.chung@gmail.com>
2025-08-19 00:28:07 +0900

    learn: math for eth transaction

--- FundMe.sol
+++ FundMe.sol
@@ -7,22 +7,31 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe {
 
-    uint256 public minimumUsd = 5; 
+    uint256 public minimumUsd = 5 * 1e18; 
 
     function fund() public payable{
-        require(msg.value >= minimumUsd, "didn't send enough ETH");  // 1e18 = 1ETH = 1* 10^18 
+        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH");  // 1e18 = 1ETH = 1* 10^18 
     }
 
 
 
     //function withdraw() public {}
 
-    function getPrice() public {
-        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306. (ETH/USD)
+    function getPrice() public view returns(uint256) {
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306 (ETH/USD)
         // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        // 5000.00000000
+        return uint256(price * 1e10);
 
     }
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // multiply first
+        return ethAmountInUsd;
+    }
 
     function getVersion() public view returns (uint256) {
         
